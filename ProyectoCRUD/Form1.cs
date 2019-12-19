78d34b18fd165f40d2e42233aae5b8274c81429a
alejorb <luis.rodriguezbenalcazar@pucese.edu.ec>
2019-12-19 01:34:21 -0500

    cambio de prueba

--- ProyectoCRUD/Form1.cs
+++ ProyectoCRUD/Form1.cs
@@ -16,5 +16,10 @@ namespace ProyectoCRUD
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //alejo
+        }
     }
 }
