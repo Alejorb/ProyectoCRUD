3fb18bb35e8f9ae78966f9311e9e2a94c8e2f422
Alejorb <luis.rodriguez@pucese.edu.ec>
2020-02-05 13:04:26 -0500

    informe

---
+++ ProyectoCRUD/Informes/frmInformeEstudiantes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoCRUD.Informes
+{
+    public partial class frmInformeEstudiantes : Form
+    {
+        public frmInformeEstudiantes()
+        {
+            InitializeComponent();
+        }
+
+        private void frmInformeEstudiantes_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'dsEstudiantes.Estudiantes' Puede moverla o quitarla según sea necesario.
+            this.estudiantesTableAdapter.Fill(this.dsEstudiantes.Estudiantes);
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
